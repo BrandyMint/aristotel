9280c4d99893662258412f1b037a4b6ebabb0472
Danil Pismenny <danil@brandymint.ru>
2015-11-25 17:59:07 +0300

    a.png -> a.gif

--- src/art.js
+++ src/art.js
@@ -29,7 +29,7 @@ var config = _.extend({
   secureCookie: false
 }, (window['art'] || {})['config']);
 
-var IMG_REQUEST_RESOURCE = config.requestUrl || getUrl('cdn.1clickanalytics.ru/a.png');
+var IMG_REQUEST_RESOURCE = config.requestUrl || getUrl('cdn.1clickanalytics.ru/a.gif');
 var currentLocation = window.location.pathname + window.location.hash;
 var currentDomain = document.domain;
 var currentReferrer = document.referrer;
